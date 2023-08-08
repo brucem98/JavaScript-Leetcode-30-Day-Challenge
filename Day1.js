2785d73b2c32f5bcfd45b0e575546d44cfc3f2b0
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-08-08 18:58:52 +0100

    Day1 Challenge

--- Day1.js
+++ Day1.js
@@ -1,1 +1,12 @@
 // https://leetcode.com/problems/create-hello-world-function/?utm_campaign=PostD1&utm_medium=Post&utm_source=Post&gio_link_id=QPDw0kJR
+
+///anonymous function
+
+const createHelloWorld = () => {
+  return function HelloWorld() {
+    return "Hello World";
+  };
+};
+
+const f = createHelloWorld();
+f(42); // "Hello World"
