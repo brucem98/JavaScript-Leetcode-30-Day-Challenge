171136c406bcec18cc62f85e68ea5230a7658063
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-08-11 14:42:32 +0100

    Day 1 and 2 notes and alternative solutions

--- Day1.js
+++ Day1.js
@@ -1,7 +1,11 @@
 // https://leetcode.com/problems/create-hello-world-function/?utm_campaign=PostD1&utm_medium=Post&utm_source=Post&gio_link_id=QPDw0kJR
 
 ///anonymous function
+// const createHelloWorld = function() {
+//   return function
+// }
 
+// createHelloWorld is a higher order function you can call twice ()(), also uses arrow syntax
 const createHelloWorld = () => {
   return function HelloWorld() {
     return "Hello World";
@@ -10,3 +14,14 @@ const createHelloWorld = () => {
 
 const f = createHelloWorld();
 f(42); // "Hello World"
+console.log("hello");
+
+//anonymous functions do not support function hoisting
+//function closures: functions have access to variables outside their scope (closure is all about hidden state)
+// Javascript used to not support classes and people used factory functions to work around that
+// If you want to omit a return you can only do it with a single line function
+
+// Spread operator
+let a = [1, 2];
+let b = [3, 4];
+console.log([...a, ...b]);
--- Day2.js
+++ Day2.js
@@ -1,12 +1,38 @@
 // https://leetcode.com/problems/create-hello-world-function/?utm_campaign=PostD1&utm_medium=Post&utm_source=Post&gio_link_id=QPDw0kJR
 
+//function closure inner function can access n from  outer function;
+
 const createCounter = (n) => {
   return () => {
-    return n++; // HIDDEN STATE
+    return n++; // HIDDEN STATE, you can also ++n to add and then return value
   };
 };
 
 const counter = createCounter(10);
-// counter() // 10
-// counter() // 11
-// counter() // 12
+counter(); // 10
+counter(); // 11
+counter(); // 12
+
+//make function loook more like object orientated programming
+const createCounter2 = (n) => {
+  let count = n;
+
+  return () => {
+    return count++;
+  };
+};
+
+//lets translate this code into a class (ES6 supported)
+
+class Counter {
+  constructor(n) {
+    this.n = n;
+  }
+
+  increment() {
+    return ++this.n; // prefix incrementing
+  }
+}
+
+const counter3 = new Counter(10); //create an instance of the class
+counter3.increment(); // 11;
